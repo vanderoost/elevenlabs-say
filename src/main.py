f75193e7cb9a553586819f4863da1d890bb9b569
Richard van der Oost <richard@vanderoost.com>
2025-06-10 13:49:32 +0100

    Update Elevenlabs
    Update broke everything due to API changes and model / voice changes.

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ import random
 from pathlib import Path
 
 from dotenv import load_dotenv
-from elevenlabs import play, save
+from elevenlabs import Voice, play, save
 from elevenlabs.client import ElevenLabs
 
 logging.basicConfig(level=logging.ERROR)
@@ -18,41 +18,40 @@ def main():
     load_dotenv(Path(__file__).parent.parent / ".env")
 
     CACHE_DIR = (
-        Path(os.getenv("XDG_CACHE_HOME", Path(os.getenv("HOME")) / ".cache"))
+        Path(os.getenv("XDG_CACHE_HOME", Path(os.environ["HOME"]) / ".cache"))
         / "elevenlabs"
     )
     CACHE_DIR.mkdir(parents=True, exist_ok=True)
-    DEFAULT_VOICE_NAME = os.getenv("DEFAULT_VOICE_NAME", "Matilda")
+    DEFAULT_VOICE_NAME = os.getenv("DEFAULT_VOICE_NAME", "Jessica")
 
     client = ElevenLabs(api_key=os.getenv("ELEVENLABS_API_KEY"))
 
     voices = get_voices(client, CACHE_DIR)
-    voice_names = [voice.name for voice in voices if type(voice.name) is str]
 
-    args = parse_args(["Any", "Male", "Female", "All"] + voice_names)
+    args = parse_args(["Any", "Male", "Female", "All"] + [str(v.name) for v in voices])
     if args.debug:
         logger.setLevel(logging.DEBUG)
 
-    logger.debug(f"Voices: {voice_names}")
+    logger.debug(f"Voices: {[v.name for v in voices]}")
 
     text = " ".join(args.text)
     model_id = get_latest_model(client, CACHE_DIR)
 
     if args.voice == "All":
         success = False
-        for voice in voice_names:
-            logger.debug(voice)
+        for voice in voices:
+            logger.debug(voice.name)
             success = say(client, text, voice, model_id, CACHE_DIR)
 
         return 0 if success else 1
 
     voice_choices = None
     if args.voice == "Any":
-        voice_choices = voice_names
+        voice_choices = [v.name for v in voices]
 
     if args.voice in {"Male", "Female"}:
         voice_choices = [
-            v.name
+            v
             for v in voices
             if v.labels and v.labels.get("gender") == args.voice.lower()
         ]
@@ -60,9 +59,10 @@ def main():
     if voice_choices:
         voice = random.choice(voice_choices)
     else:
-        voice = args.voice or DEFAULT_VOICE_NAME
+        voice_name = args.voice or DEFAULT_VOICE_NAME
+        voice = next(v for v in voices if v.name == voice_name)
 
-    assert type(voice) is str
+    assert type(voice) is Voice
 
     success = say(client, text, voice, model_id, CACHE_DIR)
 
@@ -78,7 +78,8 @@ def get_voices(client: ElevenLabs, cache_dir: Path):
             voices = pickle.load(fp)
     else:
         logger.debug("Fetching voices from API")
-        voices = client.voices.get_all().voices
+        voices = client.voices.search().voices
+
         with open(voices_path, "wb") as fp:
             pickle.dump(voices, fp)
 
@@ -106,19 +107,33 @@ def get_latest_model(client: ElevenLabs, cache_dir: Path):
     else:
         logger.debug("Fetching models from API")
         try:
-            models = client.models.get_all()
-        except:
+            models = client.models.list()
+        except Exception as e:
+            logger.error(f"Failed to fetch models: {e}")
             models = []
         with open(models_path, "wb") as fp:
             pickle.dump(models, fp)
 
-    return models[0].model_id if len(models) else "eleven_monolingual_v1"
+    logger.info(f"Found {len(models)} models")
+    logger.debug({m.model_id for m in models})
+
+    flash_models = [m for m in models if "flash" in m.model_id]
+    logger.info(f"Found {len(flash_models)} flash models")
+    logger.debug({m.model_id for m in flash_models})
+
+    if flash_models:
+        return flash_models[0].model_id
+
+    if models:
+        return models[0].model_id
+
+    return "eleven_flash_v2_5"
 
 
-def say(client: ElevenLabs, text: str, voice: str, model_id: str, cache_dir: Path):
-    logger.debug(f"Picking voice: {voice}")
+def say(client: ElevenLabs, text: str, voice: Voice, model_id: str, cache_dir: Path):
+    logger.debug(f"Picking voice: {voice.name}")
 
-    key = f"{voice}:{text}"
+    key = f"{voice.name}:{text}"
     hash = hashlib.sha256(key.encode()).hexdigest()
 
     filepath = cache_dir / "audio" / hash
@@ -128,7 +143,12 @@ def say(client: ElevenLabs, text: str, voice: str, model_id: str, cache_dir: Pat
         logger.debug("Generating and playing audio")
 
         try:
-            audio = client.generate(text=text, voice=voice, model=model_id)
+            # audio = client.generate(text=text, voice=voice, model=model_id)
+            audio = client.text_to_speech.convert(
+                text=text,
+                voice_id=voice.voice_id,
+                model_id=model_id,
+            )
             save(audio, str(filepath))
 
         except Exception as e:
@@ -143,7 +163,7 @@ def say(client: ElevenLabs, text: str, voice: str, model_id: str, cache_dir: Pat
         with open(filepath, "rb") as fp:
             play(fp)
 
-    except Exception:
+    except Exception as e:
         logger.error("Failed to play audio")
         logger.exception(e)
         logger.debug("Removing cache audio file")
